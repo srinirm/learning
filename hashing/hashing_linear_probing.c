c5dfc670a1f698f3d1bb888e76665b1fc1d7a6d1
srinirm <41440384+srinirm@users.noreply.github.com>
2020-10-10 20:39:55 +0530

    Update hashing_linear_probing.c

--- hashing/hashing_linear_probing.c
+++ hashing/hashing_linear_probing.c
@@ -99,3 +99,38 @@ void display() {
 	
    printf("\n");
 }
+
+
+int main() {
+   dummyItem = (struct DataItem*) malloc(sizeof(struct DataItem));
+   dummyItem->data = -1;  
+   dummyItem->key = -1; 
+
+   insert(1, 20);
+   insert(2, 70);
+   insert(42, 80);
+   insert(4, 25);
+   insert(12, 44);
+   insert(14, 32);
+   insert(17, 11);
+   insert(13, 78);
+   insert(37, 97);
+
+   display();
+   item = search(37);
+
+   if(item != NULL) {
+      printf("Element found: %d\n", item->data);
+   } else {
+      printf("Element not found\n");
+   }
+
+   delete(item);
+   item = search(37);
+
+   if(item != NULL) {
+      printf("Element found: %d\n", item->data);
+   } else {
+      printf("Element not found\n");
+   }
+}
